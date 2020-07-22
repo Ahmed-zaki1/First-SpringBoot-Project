2eac226f86d95059becc76ad8f9f2b0f26a83150
Ahmed <zaki40886@gmail.com>
2020-07-22 20:09:54 +0530

    Today

--- src/main/java/com/in28minutes/springboot/Controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/Controller/SurveyController.java
@@ -1,11 +1,15 @@
 package com.in28minutes.springboot.Controller;
 
+import java.net.URI;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.service.SurveyService;
@@ -19,10 +23,28 @@ private SurveyService surveyService;
 public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId){
 	return surveyService.retrieveQuestions(surveyId);
 }
+@PostMapping("/surveys/{surveyId}/questions")
+public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
 
+	Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+	if (question == null)
+		return ResponseEntity.noContent().build();
+	// Success - URI of the new resource in Response Header
+			// Status - created
+			// URI -> /surveys/{surveyId}/questions/{questionId}
+			// question.getQuestionId()
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+					"/{id}").buildAndExpand(question.getId()).toUri();
+
+			// Status
+			return ResponseEntity.created(location).build();
+}
 @GetMapping("/surveys/{surveyId}/questions/{questionId}")
 public Question retrieveQuestionsForQuestion(@PathVariable String surveyId , @PathVariable String questionId){
 	 return surveyService.retrieveQuestion(surveyId, questionId);
-}
+
 
 }
+
+}	
