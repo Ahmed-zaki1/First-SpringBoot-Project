783a1fda2fa0f95105d8dd1d9fa967aed5c69cd7
Ahmed <zaki40886@gmail.com>
2020-07-27 19:02:19 +0530

    Deep Dive Finished

--- src/test/java/com/in28minutes/springboot/Controller/SurveyControllerTest.java
+++ src/test/java/com/in28minutes/springboot/Controller/SurveyControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -26,6 +27,7 @@ import com.in28minutes.springboot.service.SurveyService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = SurveyController.class)
+@WithMockUser
 public class SurveyControllerTest {
 
 	@Autowired
@@ -51,7 +53,7 @@ public class SurveyControllerTest {
 
 		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
 
-		String expected = "{id:Question1,description:Largest Country in the World,correctAnswer:Russia}";
+		String expected = "{id:Question1,description:\"Largest Country in the World\",correctAnswer:Russia}";
 
 		JSONAssert.assertEquals(expected, result.getResponse()
 				.getContentAsString(), false);
